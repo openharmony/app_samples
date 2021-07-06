cc42fbd990d3e83b8f299e9b318bc9b1af878c99
gaohui <gaohui34@huawei.com>
2021-07-06 18:27:13 +0800

    add sample for AIFunctionSet
    Signed-off-by: gaohui <gaohui34@huawei.com>

---
+++ AI/AIFunctionSet/entry/src/main/java/ohos/samples/aifunctionset/slice/PartOfSpeechTaggingSlice.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ohos.samples.aifunctionset.slice;
+
+import ohos.samples.aifunctionset.ResourceTable;
+
+import ohos.agp.components.Component;
+import ohos.agp.components.LayoutScatter;
+import ohos.agp.components.TextField;
+import ohos.ai.nlu.NluClient;
+import ohos.ai.nlu.NluRequestType;
+import ohos.ai.nlu.ResponseResult;
+import ohos.utils.zson.ZSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Part Of Speech Tagging Slice
+ */
+public class PartOfSpeechTaggingSlice extends BaseSlice {
+    private TextField inputText;
+
+    private TextField outText;
+
+    private boolean initEngineResult;
+
+    @Override
+    public void initLayout() {
+        currComponent = LayoutScatter.getInstance(this)
+            .parse(ResourceTable.Layout_word_segmentation_slice, null, false);
+        rootLayout.addComponent(currComponent);
+
+        initEngine();
+        initComponents();
+    }
+
+    private void initEngine() {
+        NluClient.getInstance().init(this, result -> initEngineResult = true, true);
+    }
+
+    private void initComponents() {
+        outText = (TextField) findComponentById(ResourceTable.Id_out_text);
+        inputText = (TextField) findComponentById(ResourceTable.Id_input_text);
+        Component startButton = findComponentById(ResourceTable.Id_start_parse);
+        startButton.setClickedListener(this::start);
+        inputText.setText("I want to see a movie about speed and passion.");
+    }
+
+    private void start(Component component) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("text", inputText.getText());
+        map.put("type", 0);
+        String requestJson = ZSONObject.toZSONString(map);
+        if (initEngineResult) {
+            ResponseResult responseResult = NluClient.getInstance()
+                .getWordPos(requestJson, NluRequestType.REQUEST_TYPE_LOCAL);
+            if (responseResult != null) {
+                String result = responseResult.getResponseResult();
+                outText.setText("Result:" + System.lineSeparator() + result);
+            }
+        }
+    }
+
+    @Override
+    public void setTitle() {
+        aiTitle.setText("Part of Speech Tagging");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        NluClient.getInstance().destroy(this);
+    }
+}
---
+++ AI/AIFunctionSet/entry/src/main/java/ohos/samples/aifunctionset/slice/TextToSpeechSlice.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ohos.samples.aifunctionset.slice;
+
+import ohos.samples.aifunctionset.ResourceTable;
+import ohos.samples.aifunctionset.utils.LogUtil;
+
+import ohos.agp.components.Component;
+import ohos.agp.components.LayoutScatter;
+import ohos.agp.components.TextField;
+import ohos.ai.tts.TtsClient;
+import ohos.ai.tts.TtsListener;
+import ohos.ai.tts.TtsParams;
+import ohos.ai.tts.constants.TtsEvent;
+import ohos.eventhandler.EventHandler;
+import ohos.eventhandler.EventRunner;
+import ohos.eventhandler.InnerEvent;
+import ohos.utils.PacMap;
+
+import java.util.UUID;
+
+/**
+ * Text To Speech Slice
+ */
+public class TextToSpeechSlice extends BaseSlice {
+    private static final String TAG = SpeechRecognitionSlice.class.getSimpleName();
+
+    private static final int EVENT_MSG_INIT = 0x1000001;
+
+    private boolean initItsResult;
+
+    private TextField inputText;
+
+    private EventHandler handler = new EventHandler(EventRunner.current()) {
+        @Override
+        protected void processEvent(InnerEvent event) {
+            initTTSEngine();
+        }
+    };
+
+    @Override
+    public void initLayout() {
+        currComponent = LayoutScatter.getInstance(this).parse(ResourceTable.Layout_text_to_speech_slice, null, false);
+        rootLayout.addComponent(currComponent);
+
+        initTTSEngine();
+        initComponents();
+    }
+
+    @Override
+    public void setTitle() {
+        aiTitle.setText("Text To Speech");
+    }
+
+    private void initComponents() {
+        inputText = (TextField) findComponentById(ResourceTable.Id_input_text);
+        Component startPlay = findComponentById(ResourceTable.Id_start_play);
+        startPlay.setClickedListener(this::startPlay);
+    }
+
+    private void startPlay(Component component) {
+        if (initItsResult) {
+            TtsClient.getInstance().speakText(inputText.getText(), null);
+        } else {
+            showTips("InitTTSEngine Failed");
+        }
+    }
+
+    private void initTTSEngine() {
+        TtsClient.getInstance().create(this, ttsListener);
+        TtsParams ttsParams = new TtsParams();
+        ttsParams.setDeviceId(UUID.randomUUID().toString());
+        initItsResult = TtsClient.getInstance().init(ttsParams);
+        if (initItsResult) {
+            showTips("InitTTSEngine Succeeded");
+        } else {
+            handler.sendEvent(EVENT_MSG_INIT, 1000);
+        }
+    }
+
+    private TtsListener ttsListener = new TtsListener() {
+        @Override
+        public void onEvent(int eventType, PacMap pacMap) {
+            if (eventType == TtsEvent.CREATE_TTS_CLIENT_SUCCESS) {
+                LogUtil.info(TAG, "TTS Client create succeeded");
+            }
+        }
+
+        @Override
+        public void onStart(String utteranceId) {
+            LogUtil.info(TAG, utteranceId + " audio synthesis begins");
+        }
+
+        @Override
+        public void onProgress(String utteranceId, byte[] audioData, int progress) {
+            LogUtil.info(TAG, utteranceId + " audio synthesis progress：" + progress);
+        }
+
+        @Override
+        public void onFinish(String utteranceId) {
+            LogUtil.info(TAG, utteranceId + " audio synthesis completed");
+        }
+
+        @Override
+        public void onSpeechStart(String utteranceId) {
+            LogUtil.info(TAG, utteranceId + " begins to speech");
+        }
+
+        @Override
+        public void onSpeechProgressChanged(String utteranceId, int progress) {
+            LogUtil.info(TAG, utteranceId + " speech progress：" + progress);
+        }
+
+        @Override
+        public void onSpeechFinish(String utteranceId) {
+            LogUtil.info(TAG, utteranceId + " speech completed");
+        }
+
+        @Override
+        public void onError(String utteranceId, String errorMessage) {
+            LogUtil.info(TAG, utteranceId + " errorMessage: " + errorMessage);
+        }
+    };
+}
