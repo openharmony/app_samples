fe5c188f645d884707c43d0ad8149ae6e97e5674
gaohui <gaohui34@huawei.com>
2021-07-15 15:17:07 +0800

    add sample for RefreshContainer
    Signed-off-by: gaohui <gaohui34@huawei.com>

---
+++ UI/RefreshContainer/entry/src/main/js/default/pages/index/index.js
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export default {
+    data: {
+        list_item: [
+            {
+                title: " "
+            },
+            {
+                title: " "
+            },
+            {
+                title: " "
+            },
+            {
+                title: " "
+            }
+        ]
+    },
+    onInit() {
+        this.initData()
+    },
+    refreshData: function (refreshing) {
+        if ("end" == refreshing.state) {
+            for (var index = 0; index < 2; index++) {
+                var item = {
+                    title: this.$t('strings.title') + (this.list_item.length + 1)
+                }
+                this.list_item.splice(0, 0, item)
+            }
+        }
+    },
+    initData: function (params) {
+        var index = this.list_item.length
+        for (let todolistKey in this.list_item) {
+            this.list_item[todolistKey].title = this.$t('strings.title') + index
+            index--
+        }
+    }
+}
