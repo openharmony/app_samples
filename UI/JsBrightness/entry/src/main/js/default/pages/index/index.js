cc2f046403e683efa8d1413a0d39c71afbe3072a
caopan <caopan8@huawei.com>
2021-09-07 17:34:04 +0800

    the add sample for sets the screen brightness
    Signed-off-by: caopan <caopan8@huawei.com>

--- UI/JsBrightness/entry/src/main/js/default/pages/index/index.js
+++ UI/JsBrightness/entry/src/main/js/default/pages/index/index.js
@@ -17,36 +17,6 @@ import brightness from '@ohos.brightness';
 
 export default {
     brightness(e) {
-        switch (e.value) {
-            case 0:
-            brightness.setValue(0);
-            break;
-            case 30:
-            brightness.setValue(30);
-            break;
-            case 60:
-            brightness.setValue(60);
-            break;
-            case 90:
-            brightness.setValue(90);
-            break;
-            case 120:
-            brightness.setValue(120);
-            break;
-            case 150:
-            brightness.setValue(150);
-            break;
-            case 180:
-            brightness.setValue(180);
-            break;
-            case 210:
-            brightness.setValue(210);
-            break;
-            case 240:
-            brightness.setValue(240);
-            break;
-            default:
-                break;
-        }
+        brightness.setValue(e.value);
     }
 }
