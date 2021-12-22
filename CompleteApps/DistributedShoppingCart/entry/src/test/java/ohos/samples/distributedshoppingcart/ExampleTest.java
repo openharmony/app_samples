12251ef1a6d2c32a68ec902f0cd7e8fdfe68917b
guojin26 <guojin26@huawei.com>
2021-12-22 03:16:14 +0000

    !256 删除Openharmony下的L3 sample * delete L3 samples

--- CompleteApps/DistributedShoppingCart/entry/src/test/java/ohos/samples/distributedshoppingcart/ExampleTest.java
+++
@@ -1,9 +0,0 @@
-package ohos.samples.distributedshoppingcart;
-
-import org.junit.Test;
-
-public class ExampleTest {
-    @Test
-    public void onStart() {
-    }
-}
--- CompleteApps/TicTacToeGame/entry/src/test/java/ohos/samples/tictactoegame/ExampleTest.java
+++
@@ -1,9 +0,0 @@
-package ohos.samples.tictactoegame;
-
-import org.junit.Test;
-
-public class ExampleTest {
-    @Test
-    public void onStart() {
-    }
-}
