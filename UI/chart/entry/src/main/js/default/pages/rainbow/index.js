00674de10431832235a8741037c7cca198dabb3b
adslk <liuxin283@huawei.com>
2022-01-20 07:03:30 +0000

    !296 BUG-[Demo&sample应用子系统]【Openharmony】修改chart占比图表颜色不匹配问题 * bugfix the chart

--- UI/chart/entry/src/main/js/default/pages/rainbow/index.js
+++ UI/chart/entry/src/main/js/default/pages/rainbow/index.js
@@ -17,18 +17,28 @@ export default {
     data: {
         segment: [
             {
+                startColor: "#f7ce00",
+                endColor: "#f99b11",
                 value: 40
             },
             {
+                startColor: "#f76223",
+                endColor: "#f2400a",
                 value: 20
             },
             {
+                startColor: "#f772ac",
+                endColor: "#e65392",
                 value: 10
             },
             {
+                startColor: "#a575eb",
+                endColor: "#a12df7",
                 value: 15
             },
             {
+                startColor: "#7b79f7",
+                endColor: "#4b48f7",
                 value: 15
             }
         ]
