aaf9cf8d570d9a5b694e1b391c0aa7794232ccb6
keerecles <lishihao4@huawei.com>
2022-03-31 11:58:43 +0000

    !485 XComponent Ets Sample for OH Release3.1 * XComponent Sample Release3.1

--- ETSUI/eTSXComponent/entry/src/main/cpp/common/plugin_common.h
+++ ETSUI/eTSXComponent/entry/src/main/cpp/common/plugin_common.h
@@ -18,9 +18,9 @@
 
 #include <Hilog/log.h>
 
-#define LOGI(...) ((void)HiLogPrint(LOG_APP, LOG_INFO, LOG_DOMAIN, "HMG_LOG", __VA_ARGS__))
-#define LOGD(...) ((void)HiLogPrint(LOG_APP, LOG_DEBUG, LOG_DOMAIN, "HMG_LOG", __VA_ARGS__))
-#define LOGW(...) ((void)HiLogPrint(LOG_APP, LOG_WARN, LOG_DOMAIN, "HMG_LOG", __VA_ARGS__))
-#define LOGE(...) ((void)HiLogPrint(LOG_APP, LOG_ERROR, LOG_DOMAIN, "HMG_LOG", __VA_ARGS__))
+#define LOGI(...) ((void)OH_LOG_Print(LOG_APP, LOG_INFO, LOG_DOMAIN, "HMG_LOG", __VA_ARGS__))
+#define LOGD(...) ((void)OH_LOG_Print(LOG_APP, LOG_DEBUG, LOG_DOMAIN, "HMG_LOG", __VA_ARGS__))
+#define LOGW(...) ((void)OH_LOG_Print(LOG_APP, LOG_WARN, LOG_DOMAIN, "HMG_LOG", __VA_ARGS__))
+#define LOGE(...) ((void)OH_LOG_Print(LOG_APP, LOG_ERROR, LOG_DOMAIN, "HMG_LOG", __VA_ARGS__))
 
 #endif // _PLUGIN_COMMON_H_
