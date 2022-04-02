7eb9bb2d721bdf75ecb98305cffa411a46ea9741
liuxin256 <liuxin283@huawei.com>
2022-04-02 08:32:52 +0000

    !510 设置系统时间增加相关权限 * add the SET_TIME permission

--- UI/JsTimer/entry/src/main/js/default/pages/index/index.js
+++ UI/JsTimer/entry/src/main/js/default/pages/index/index.js
@@ -20,18 +20,9 @@ import systemTime from '@ohos.systemTime';
 export default {
     timer: undefined,
     data: {
+        multiTextValue: [],
         dataDateTime: '',
         dataTime: '00:00:00',
-        multiText: [["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17",
-        "18", "19", "20", "21", "22", "23", "24"],
-        ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17",
-        "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35",
-        "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53",
-        "54", "55", "56", "57", "58", "59"],
-        ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17",
-        "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35",
-        "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "53",
-        "54", "55", "56", "57", "58", "59"]],
         timeoutID: '0',
         hour: 0,
         minute: 0,
@@ -43,19 +34,19 @@ export default {
         time: 0
     },
     onInit() {
-        var date = new Date()
+        let date = new Date()
         this.dataDateTime = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + "-" +
         date.getHours() + "-" + date.getMinutes()
     },
     dateTimeChange(e) {
         this.dataDateTime = e.year + '-' + (e.month + 1) + '-' + e.day + '-' + e.hour + '-' + e.minute;
-        var date = new Date(e.year, e.month, e.day, e.hour, e.minute);
+        let date = new Date(e.year, e.month, e.day, e.hour, e.minute);
         this.time = date.getTime();
     },
     multiTextChange(e) {
-        this.hour = e.newValue[0]
-        this.minute = e.newValue[1]
-        this.second = e.newValue[2]
+        this.hour = e.hour
+        this.minute = e.minute
+        this.second = e.second
         this.hourStr = this.hour + ''
         this.minuteStr = this.minute + ''
         this.secondStr = this.second + ''
@@ -69,13 +60,14 @@ export default {
             this.secondStr = '0' + this.second
         }
         this.dataTime = this.hourStr + ':' + this.minuteStr + ':' + this.secondStr;
+        console.log(`[JsTimer] update dataTime=`+ this.dataTime)
         this.milliSecond = this.hour * 3600 * 1000 + this.minute * 60 * 1000 +
         this.second * 1000;
     },
     updateTime: function () {
-        var hper = 60 * 60 * 1000
-        var mper = 60 * 1000
-        var ts = this.milliSecond
+        let hper = 60 * 60 * 1000
+        let mper = 60 * 1000
+        let ts = this.milliSecond
         this.hour = parseInt(ts / hper)
         this.hourStr = this.hour + ''
         if (ts / hper < 10) {
@@ -102,11 +94,13 @@ export default {
         }
     },
     setTimer() {
+        console.log(`[JsTimer] enter setTimer`)
         this.timer = setInterval(this.updateTime, 1000)
+        console.log(`[JsTimer] dataTime= `+ JSON.stringify(this.dataTime))
         prompt.showToast({
             message: this.$t('strings.success'),
         });
-        var message = this.$t('strings.shutdown');
+        let message = this.$t('strings.shutdown');
         this.timeoutID = setTimeout(function () {
             prompt.showToast({
                 message: message,
@@ -123,11 +117,12 @@ export default {
         });
     },
     setTime() {
+        console.log(`[JsTimer] this.time= ${this.time}`)
         systemTime.setTime(this.time)
             .then((value) => {
-                console.log(`success to systemTime.setTime: ${value}`);
+                console.log(`[JsTimer] success to systemTime.setTime: ${value}`);
             }).catch((err) => {
-            console.error(`failed to systemTime.setTime because ${err.message}`);
+            console.error(`[JsTimer] failed to systemTime.setTime because ${err.message}`);
         });
     }
 };
