922878e64ec5dad65d4dcc7305def272e69abf23
zhangxinlogo <zhangxin360@huawei.com>
2022-06-09 06:48:36 +0000

    !788 新增npm * README_zh.md * update * add npm

---
+++ EngineeringCapability/Npm/entry/src/main/ets/MainAbility/MainAbility.ts
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import Ability from '@ohos.application.Ability'
+import logger from '../model/Logger'
+
+const TAG: string = 'MainAbility'
+
+export default class MainAbility extends Ability {
+  onCreate(want, launchParam) {
+    logger.info(TAG, `[Demo] MainAbility onCreate`)
+    globalThis.abilityWant = want
+    globalThis.abilityContext = this.context
+  }
+
+  onDestroy() {
+    logger.info(TAG, `[Demo] MainAbility onDestroy`)
+  }
+
+  onWindowStageCreate(windowStage) {
+    // Main window is created, set main page for this ability
+    logger.info(TAG, `[Demo] MainAbility onWindowStageCreate`)
+
+    windowStage.setUIContent(this.context, "pages/Index", null)
+  }
+
+  onWindowStageDestroy() {
+    // Main window is destroyed, release UI related resources
+    logger.info(TAG, `[Demo] MainAbility onWindowStageDestroy`)
+  }
+
+  onForeground() {
+    // Ability has brought to foreground
+    logger.info(TAG, `[Demo] MainAbility onForeground`)
+  }
+
+  onBackground() {
+    // Ability has back to background
+    logger.info(TAG, `[Demo] MainAbility onBackground`)
+  }
+}
