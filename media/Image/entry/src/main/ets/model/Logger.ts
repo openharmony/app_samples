c36e16fae0ce37353aed3f484fd43994c2144f66
zhangfei113 <zhangfei113@huawei.com>
2022-06-21 02:47:51 +0000

    !861 适配64位 * update 901

--- media/Image/entry/src/main/ets/model/Logger.ts
+++ media/Image/entry/src/main/ets/model/Logger.ts
@@ -12,33 +12,34 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import hilog from '@ohos.hilog'
 
 class Logger {
-    private domain: number
-    private prefix: string
-    private format: string = '%{public}s, %{public}s'
+  private domain: number
+  private prefix: string
+  private format: string = '%{public}s, %{public}s'
 
-    constructor(prefix: string) {
-        this.prefix = prefix
-        this.domain = 0xFF00
-    }
+  constructor(prefix: string) {
+    this.prefix = prefix
+    this.domain = 0xFF00
+  }
 
-    debug(...args: any[]) {
-        hilog.debug(this.domain, this.prefix, this.format, args)
-    }
+  debug(...args: any[]) {
+    hilog.debug(this.domain, this.prefix, this.format, args)
+  }
 
-    info(...args: any[]) {
-        hilog.info(this.domain, this.prefix, this.format, args)
-    }
+  info(...args: any[]) {
+    hilog.info(this.domain, this.prefix, this.format, args)
+  }
 
-    warn(...args: any[]) {
-        hilog.warn(this.domain, this.prefix, this.format, args)
-    }
+  warn(...args: any[]) {
+    hilog.warn(this.domain, this.prefix, this.format, args)
+  }
 
-    error(...args: any[]) {
-        hilog.error(this.domain, this.prefix, this.format, args)
-    }
+  error(...args: any[]) {
+    hilog.error(this.domain, this.prefix, this.format, args)
+  }
 }
 
 export default new Logger('[ImageProcessing]')
