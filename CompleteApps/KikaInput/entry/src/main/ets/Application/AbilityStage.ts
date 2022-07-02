6464e1aca516f4ae5db234426e3ea8e29b8ff241
三辰 <liuao14@huawei.com>
2022-07-02 10:26:50 +0000

    !945 kikaInput输入法在横屏下的呼出与正常显示 Merge pull request !945 from 三辰/master

--- CompleteApps/KikaInput/entry/src/main/ets/Application/AbilityStage.ts
+++ CompleteApps/KikaInput/entry/src/main/ets/Application/AbilityStage.ts
@@ -1,7 +1,9 @@
 import AbilityStage from "@ohos.application.AbilityStage"
 
 export default class MyAbilityStage extends AbilityStage {
-    onCreate() {
-        console.log("[Demo] MyAbilityStage onCreate")
-    }
+  onCreate() {
+    AppStorage.SetOrCreate<number>('windowWidth', -1)
+    AppStorage.SetOrCreate<number>('windowHeight', -1)
+    console.log("[Demo] MyAbilityStage onCreate")
+  }
 }
